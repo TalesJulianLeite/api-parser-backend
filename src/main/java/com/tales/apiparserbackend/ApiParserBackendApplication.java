6d355885c464d3ca3624d7caf4789287220e4e48
TALESJULIAN <talesjulianl@gmail.com>
2019-05-31 18:34:16 -0300

    Initial commit (create project)

---
+++ src/main/java/com/tales/apiparserbackend/ApiParserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tales.apiparserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParserBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tales/apiparserbackend/ApiParserBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tales.apiparserbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiParserBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
