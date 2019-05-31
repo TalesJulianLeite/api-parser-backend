cb25f620559427fffdac0a45cfe67b21306eab06
TALESJULIAN <talesjulianl@gmail.com>
2019-05-31 20:37:31 -0300

    Create Entitites

---
+++ src/main/java/com/tales/apiparserbackend/entities/Game.java
@@ -0,0 +1,102 @@
+package com.tales.apiparserbackend.entities;
+
+import java.beans.Transient;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "game")
+public class Game implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4436419272875921363L;
+	
+	private Long id;
+	private Integer number;
+	private Integer total_kills;
+	List<Player> players;
+	private boolean game_shutdown;
+	
+	public Game() {
+		
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @param Nnumber
+	 * @param total_kills
+	 * @param players
+	 */
+	public Game(Integer game_number, Integer total_kills, List<Player> players) {
+		this.number = game_number;
+		this.total_kills = total_kills;
+		this.players = players;
+	}
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name="number", nullable = false)
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer game_number) {
+		this.number = game_number;
+	}
+
+	@Column(name="total_kills", nullable = false)
+	public Integer getTotal_kills() {
+		return total_kills;
+	}
+	
+	public void setTotal_kills(Integer total_kills) {
+		this.total_kills = total_kills;
+	}
+	
+	@OneToMany(mappedBy= "game", fetch= FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Player> getPlayers() {
+		return players;
+	}
+	
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+
+	@Transient
+	public boolean isGame_shutdown() {
+		return game_shutdown;
+	}
+
+	public void setGame_shutdown(boolean game_shutdown) {
+		this.game_shutdown = game_shutdown;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [number=" + number + ", total_kills=" + total_kills + "]";
+	}	
+	
+}
---
+++ src/main/java/com/tales/apiparserbackend/entities/Player.java
@@ -0,0 +1,87 @@
+package com.tales.apiparserbackend.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="player")
+public class Player implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1038869971280729101L;
+	private Long id;
+	private Integer number;
+	private String name;
+	private Integer kills;
+	private Game game;
+	
+	
+	public Player() {
+		
+	}
+		
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	@Column(name = "number", nullable = false)
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer player_number) {
+		this.number = player_number;
+	}
+
+	@Column(name = "name", nullable = false)
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "kills", nullable = true)
+	public Integer getKills() {
+		return kills;
+	}
+	
+	public void setKills(Integer kills) {
+		this.kills = kills;
+	}
+
+	@ManyToOne(fetch= FetchType.EAGER)
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [number=" + number + ", name=" + name + ", kills=" + kills + "]";
+	}
+	
+
+}
